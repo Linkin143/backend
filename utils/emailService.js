53353fe2272e966fd6e2b1007ec91dc525ab19b0
Linkin143 <kumarprince143p143@gmail.com>
2026-02-07 15:49:46 +0530

    smtp updated

--- utils/emailService.js
+++ utils/emailService.js
@@ -7,16 +7,14 @@ dotenv.config();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, 
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS // No spaces here
-    },
-    tls: {
-        rejectUnauthorized: false // Helps bypass cloud network restrictions
-    }
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  family: 4, 
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 // Replace this with your actual hosted logo URL
